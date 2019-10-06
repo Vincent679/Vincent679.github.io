cea4b20aec0de54e4860c05d8852ceb32218d3d3
Vincent Ivanov <54909869+Vincent679@users.noreply.github.com>
2019-10-06 05:43:42 +0200

    Add files via upload

---
+++ standart/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.move__carousel').slick({
+        centerMode: true,
+        // centerPadding: '60px',
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        variableWidth: true
+    });
+  });
